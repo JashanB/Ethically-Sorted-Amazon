ea3ef45a03c5afe8597cc52ab64eff3a339ccc3b
JashanB <jashan_brar50@hotmail.com>
2020-04-13 21:26:43 -0400

    adding states

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import Search from '../search'
 //class="sg-col-inner"
 //class="s-result-list s-search-results sg-row"
 function App() {
+  const [search, setSearch] = useState("")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search 
+        search={search}
+        setSearch={setSearch}
+      />
     </div>
   );
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,7 +5,11 @@ const handleSubmit = (event) => {
 }
 
 export default function Search(props) {
-  //search form that splits input by spaces 
+  //search form that splits input by spaces and makes axios call 
+  //https://www.amazon.com/s?k=hand+cream&page=3&qid=1586824474&ref=sr_pg_3
+  //https://www.amazon.com/s?k=hand+cream&page=3 works too - only need this information 
+  //can set this to go to like 50 but only function if data returned 
+  const [error, setError] = useState("")
   return (
     <div>
       <form onSubmit={handleSubmit}>
